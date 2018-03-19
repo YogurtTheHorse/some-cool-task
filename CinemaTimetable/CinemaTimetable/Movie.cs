83e368edd8af03dde5d2253a078cb93805022f36
yegorf1 <yegorf1@gmail.com>
2018-03-19 12:26:55 +0300

    Implemented Movie class

--- CinemaTimetable/CinemaTimetable/Movie.cs
+++ CinemaTimetable/CinemaTimetable/Movie.cs
@@ -1,8 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
 
 namespace CinemaTimetable {
 	public class Movie {
+		public string MovieName { get; }
+		public TimeSpan Duration { get; }
+		public int Year { get; }
+
+		public MovieRating Rating { get; }
+
+		public RegionInfo[] ProductionCountries { get; }
+
+		public Movie(string name, TimeSpan duration) : this(name, duration, 0) { }
+
+		public Movie(string name, TimeSpan duration, int year) : this(name, duration, year, MovieRating.Unknown, new RegionInfo[] { }) { }
+
+		public Movie(string name, TimeSpan duration, int year, MovieRating rating, RegionInfo[] regions) {
+			MovieName = name;
+			Duration = duration;
+			Year = year;
+			Rating = rating;
+			ProductionCountries = regions;
+		}
+
+		public override string ToString() {
+			return $"{MovieName} {Year}";
+		}
 	}
 }
