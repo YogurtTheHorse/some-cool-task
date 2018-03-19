1bc211bd9bb9d70c9e38d2c70cca50db99fe6205
Ilya Burakov <speedwatson@gmail.com>
2018-03-19 12:34:27 +0300

    MovieShow implementation

--- CinemaTimetable/CinemaTimetable/MovieShow.cs
+++ CinemaTimetable/CinemaTimetable/MovieShow.cs
@@ -5,6 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CinemaTimetable {
-    class MovieShow {
+    public class MovieShow: IEquatable<MovieShow> {
+        public Movie Movie { get; set; }
+        int Hall { get; set; }
+
+        DateTime Date;
+        DayOfWeek DayOfWeek { get => Date.DayOfWeek; }
+         
+
+        public MovieShow(Movie movie, DateTime date, int hall) {
+            Movie = movie;
+            Date = date;
+            Hall = hall;
+        }
+
+        public bool Equals(MovieShow second) {
+            if (second == null) {
+                return false;
+            }
+
+            return (Movie == second.Movie) && (Date == second.Date) && (Hall == second.Hall);
+        }
     }
 }
